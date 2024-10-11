e0830923ea854b92d592e21fa7c16adf883c2c70
ilyadrbn <ilyadrbn@gmail.com>
2024-10-11 19:03:50 +0300

    scrollable header

--- components/header/aside.vue
+++ components/header/aside.vue
@@ -6,7 +6,7 @@ const isMenuOpened = ref<boolean>(false);
 
 <template>
     <header
-        class="aside-header sticky left-0 top-0 h-screen flex-col border border-primary-light-3 px-6 py-8 transition-all duration-300 ease-in-out dark:border-primary-dark-3 max-lg:relative max-lg:h-20 max-lg:overflow-hidden max-lg:py-2"
+        class="aside-header sticky left-0 top-0 h-screen min-w-fit overflow-auto border border-primary-light-3 px-6 py-8 transition-all duration-300 ease-in-out dark:border-primary-dark-3 max-lg:relative max-lg:h-20 max-lg:overflow-hidden max-lg:py-2"
         :class="{
             'max-lg:h-screen max-lg:!overflow-auto max-lg:pb-8': isMenuOpened,
         }"
@@ -15,9 +15,7 @@ const isMenuOpened = ref<boolean>(false);
             :name="menuItems.name"
             :position="menuItems.position"
         />
-        <ToggleMenuBtn
-            v-model="isMenuOpened"
-        />
+        <ToggleMenuBtn v-model="isMenuOpened" />
         <div class="aside-header__wrapper flex flex-col gap-8">
             <HeaderRoutesList :routes="menuItems.routes" />
             <HeaderRoutesList
@@ -28,3 +26,9 @@ const isMenuOpened = ref<boolean>(false);
         </div>
     </header>
 </template>
+
+<style scoped>
+header.aside-header::-webkit-scrollbar {
+    display: none;
+}
+</style>
